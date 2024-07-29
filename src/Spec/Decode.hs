dc42871b4d98bd644b67bc754c784fbb89944007
Arthur De Belen <debelen.arthurreiner@gmail.com>
2024-07-29 12:30:31 -0400

    fix for parsing ambiguity between FP and V insts

--- src/Spec/Decode.hs
+++ src/Spec/Decode.hs
@@ -859,10 +859,10 @@ decode iset inst =
       | opcode==opcode_OP_V, funct3==funct3_VSETVLI, (bitSlice inst 31 32)==0b0 = Vsetvli rd rs1 zimm11
       | opcode==opcode_OP_V, funct3==funct3_VSETVLI, (bitSlice inst 30 32)==0b11 = Vsetivli rd rs1 zimm10
       | opcode==opcode_OP_V, funct3==funct3_VSETVLI, (bitSlice inst 31 32)==0b1, (bitSlice inst 25 31)==0b000000 = Vsetvl rd rs1 rs2
-      | opcode==opcode_LOAD_FP, mop==0b00, lumop==0b00000 = Vle width vd rs1 vm
-      | opcode==opcode_LOAD_FP, mop==0b00, lumop==0b01000 = Vlr vd rs1 nf
-      | opcode==opcode_STORE_FP, mop==0b00, sumop==0b00000 = Vse width vs3 rs1 vm
-      | opcode==opcode_STORE_FP, mop==0b00, lumop==0b01000 = Vsr vd rs1 nf
+      | opcode==opcode_LOAD_FP, (width==0b000 || width==0b101 || width==0b110 || width == 0b111), mop==0b00, lumop==0b00000 = Vle width vd rs1 vm
+      | opcode==opcode_LOAD_FP, (width==0b000 || width==0b101 || width==0b110 || width == 0b111), vm==0b1, mop==0b00, lumop==0b01000 = Vlr vd rs1 nf
+      | opcode==opcode_STORE_FP, (width==0b000 || width==0b101 || width==0b110 || width == 0b111),  mop==0b00, sumop==0b00000 = Vse width vs3 rs1 vm
+      | opcode==opcode_STORE_FP, vm==0b1, width==0b000, (bitSlice inst 28 29)==0b0, mop==0b00, sumop==0b01000 = Vsr vd rs1 nf
       | opcode==opcode_OP_V, width==0b000, funct6==0b000000 = Vaddvv vd rs1 vs2 vm
       | True = InvalidV
 -- ================================================================
